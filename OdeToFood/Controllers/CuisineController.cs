f711867cc055a6cbe0b645444c9e6ce8b8f1e139
Eduardo Matos <edu.de.matos@hotmail.com>
2013-11-20 20:53:38 -0200

    primeiro commit

---
+++ OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class CuisineController : Controller
+    {
+        //
+        // GET: /Cuisine/
+
+        public ActionResult Search(string search = "french")
+        {
+            var message = Server.HtmlEncode(search);
+            return Content(message);
+        }
+
+    }
+}
